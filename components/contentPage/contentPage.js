48648f05150b571f685afcfb3410fd24267017e6
utility4u <benedikthofirek@gmail.com>
2021-03-04 12:26:51 +0100

    color1

--- components/contentPage/contentPage.js
+++ components/contentPage/contentPage.js
@@ -18,6 +18,7 @@ export default function contentPage() {
       <StatisticSection />
       <ProjectsSection />
       <ContactSection />
+      <div className={styles.shadow}></div>
     </div>
   )
 }

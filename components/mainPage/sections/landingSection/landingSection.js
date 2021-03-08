69fb902e99115c6089f1b7aca368938e91aa9a76
utility4u <benedikthofirek@gmail.com>
2021-03-08 15:20:23 +0100

    wip

--- components/mainPage/sections/landingSection/landingSection.js
+++ components/mainPage/sections/landingSection/landingSection.js
@@ -1,29 +1,102 @@
 import Image from 'next/image';
-import { useRef, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './landingSection.module.scss';
 
 export default function LandingSection({
   handleScroll,
   containerClass
 }) {
-  const primaryTitle = useRef(null);
-  const secondaryTitle = useRef(null);
+  const imageAnimationTime = 2000;
+  const arrowAnimationTime = 1000;
+  const writeSpeed = 120;
 
+  const titleTextSource = "Hello, I'm Benedikt";
+  const paragraphTextSource = "I'm a full-stack developer";
+
+  const [isArrowDisplayed, setIsArrowDisplayed] = useState(false);
+  const [isImageDisplayed, setIsImageDisplayed] = useState(false);
+  const [isTitleDisplayed, setIsTitleDisplayed] = useState(false);
+  const [isParagraphDisplayed, setIsParagraphDisplayed] = useState(false);
+  const [titleText, setTitleText] = useState('');
+  const [paragraphText, setParagraphText] = useState('');
+
+  //loading animation
   useEffect(() => {
-    console.log('pruchod', primaryTitle?.current, secondaryTitle?.current);
-    if (primaryTitle?.current && secondaryTitle.current) {
-        // "Hello, I'm Benedikt"
+    (async() => {
+      // title
+      await pause(1200);
+      setIsTitleDisplayed(true);
+      await write(setTitleText, titleTextSource);
+      
+      //image
+      await pause(500);
+      setIsImageDisplayed(true);
+      await pause(imageAnimationTime);
+      await pause(500);
 
-        // "I'm a full-stack developer"
+      //paragraph
+      setIsParagraphDisplayed(true);
+      await write(setParagraphText, paragraphTextSource);
+      await pause(500);
+      setIsArrowDisplayed(true);
+    })()
+  }, []);
+
+  function pause(timeout) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, timeout);
+    });
+  }
+
+  function write(func, text, time = writeSpeed) {
+    const charList = text.split('');
+    const charListLength = charList.length;
+
+    function getTimeout() {
+      return Math.round(time);
     }
-    
-  }, [primaryTitle?.current, secondaryTitle?.current]);
+
+    function addChar(char = '') {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          func((value) => value.slice(0,-1) + char + '|');
+          resolve();
+        }, getTimeout());
+      });
+    }
+
+    return new Promise(async(resolve) => {
+      //add cursor
+      await addChar();
+
+      for (let x = 0; x < charListLength; x++) {
+        await addChar(charList[x]);
+      }
+
+      //removeCursor
+      func((value) => value.slice(0, -1));
+      resolve();
+    });
+  }
+
+  const imageAnimationStyle = {
+    animationName: 'fadeIn',
+    animationDuration: `${imageAnimationTime}ms`,
+  };
+
+  const arrowAnimationStyle = {
+    animationName: 'fadeInDown',
+    animationDuration: `${arrowAnimationTime}ms`,
+  };
 
   return (
     <div 
       className={"section " + containerClass + ' ' + styles.container}
     >
-      <div className={'image ' + styles.photoContainer}>
+      <div
+        className={'image ' + styles.photoContainer}
+        style={isImageDisplayed ? imageAnimationStyle : {}}
+      >
         <div className={styles.photo}>
           <Image
             src="/../public/Benedikt_Hofirek.jpg"
@@ -35,15 +108,24 @@ export default function LandingSection({
       <div className={styles.textContainer}>
         <h1 
           className={'sectionTitle_content sectionTitle_content__main ' + styles.title}
-          ref={primaryTitle}
-        ></h1>
+          style={{
+            display: isTitleDisplayed ? 'block' : 'none',
+          }}
+        >
+          {titleText}
+        </h1>
         <p 
           className={'paragraph ' + styles.description}
-          ref={secondaryTitle}  
-        ></p>
+          style={{
+            display: isParagraphDisplayed ? 'block' : 'none',
+          }}
+        >
+          {paragraphText}
+        </p>
       </div>
       <div
         className={styles.arrowDown}
+        style={isArrowDisplayed ? arrowAnimationStyle : {}}
         onClick={handleScroll} //scroll down  
       >
         <Image

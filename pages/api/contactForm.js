7aee2507b9422b8d0f8ce24b3a37aa691bbbc480
utility4u <benedikthofirek@gmail.com>
2023-11-23 20:47:06 +0100

    email next try

--- pages/api/contactForm.js
+++ pages/api/contactForm.js
@@ -1,4 +1,5 @@
 var nodemailer = require('nodemailer');
+var sendmail = require('sendmail')();
 
 export default (req, res) => {
   if (req.method === 'POST') {
@@ -9,34 +10,43 @@ export default (req, res) => {
       subject,
     } = req.body;
    
-    var transporter = nodemailer.createTransport({
-      service: 'smtp.seznam.cz',
-      port: 587,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD
-      }
-    });
+    // var transporter = nodemailer.createTransport({
+    //   service: 'smtp.seznam.cz',
+    //   port: 587,
+    //   secure: true,
+    //   auth: {
+    //     user: process.env.EMAIL,
+    //     pass: process.env.EMAIL_PASSWORD
+    //   }
+    // });
+    //
+    // var mailOptions = {
+    //   from: process.env.EMAIL,
+    //   to: process.env.EMAIL,
+    //   subject: 'CONTACT FORM from' + senderEmail,
+    //   text: `sender: ${senderEmail}\n
+    //          name: ${senderName}
+    //          subject: ${subject} \n
+    //          message: ${message}`
+    // };
+    //
+    // transporter.sendMail(mailOptions, function(error, info){
+    //   if (error) {
+    //     console.log('err', error);
+    //     res.status(500).end();
+    //   } else {
+    //     res.status(200).end();
+    //   }
+    // });
 
-    var mailOptions = {
-      from: process.env.EMAIL,
-      to: process.env.EMAIL,
-      subject: 'CONTACT FORM from' + senderEmail,
-      text: `sender: ${senderEmail}\n
-             name: ${senderName}
-             subject: ${subject} \n
-             message: ${message}`
-    };
+    sendmail({
+        from: senderEmail,
+        to: process.env.Email,
+        subject,
+        html: senderName + "\n\n" + message
+        }, function(err, reply) {
 
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log('err', error);
-        res.status(500).end();
-      } else {
-        res.status(200).end();
-      }
-    });
+        })
   }
 }
 
